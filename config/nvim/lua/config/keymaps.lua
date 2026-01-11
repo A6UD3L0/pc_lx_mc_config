b5e17d46c5be8fa6e0993ae0352dc2772d817126
Juan-F-Agudelo <juan.agudelo@grupofiglobal.com>
2026-01-11 18:42:45 -0500

    feat: Add buffer close and navigation keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,16 @@ local map = vim.keymap.set
 
 -- Exit insert mode with jk
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Buffer management
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+map("n", "<leader>bD", "<cmd>bdelete!<cr>", { desc = "Delete buffer (force)" })
+map("n", "<leader>bo", "<cmd>%bdelete|edit#|bdelete#<cr>", { desc = "Delete other buffers" })
+map("n", "<leader>bx", "<cmd>bdelete<cr>", { desc = "Close buffer" })
+map("n", "<S-q>", "<cmd>bdelete<cr>", { desc = "Close buffer (Shift+Q)" })
+
+-- Buffer navigation
+map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
