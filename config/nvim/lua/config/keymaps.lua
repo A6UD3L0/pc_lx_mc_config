410d261191dae8a5f233eefbcb32adbb8825d41b
A6UD3L0 <juanf.agudelor@gmail.com>
2026-01-13 07:15:35 -0500

    Cleanup: Remove all Docker/Windows artifacts and duplicate folders

--- config/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Add any additional keymaps here
--- NOTE: Molten keymaps are defined in plugins/datascience.lua
-
-local map = vim.keymap.set
-
--- Exit insert mode with jk
-map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
--- config/nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Add any additional options here
-
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.clipboard = "unnamedplus"
